e236d8905da698946a220a72f2d1a57bdd7384db
David Lienhard <43143441+davidlienhard@users.noreply.github.com>
2023-09-11 10:06:06 +0200

    be more specific on return types (#192)
    be more specific of data returned in the arrays
    the data can only be of types `int`, `float`, `string`, `bool` or `null`

--- src/MysqliResult.php
+++ src/MysqliResult.php
@@ -34,7 +34,7 @@ class MysqliResult implements ResultInterface
      *
      * @author          David Lienhard <github@lienhard.win>
      * @copyright       David Lienhard
-     * @return          mixed[]
+     * @return          (int|float|string|bool|null)[]
      * @throws          \DavidLienhard\Database\Exception if any mysqli function failed
      */
     public function fetch_assoc() : array|null
@@ -55,7 +55,7 @@ class MysqliResult implements ResultInterface
      *
      * @author          David Lienhard <github@lienhard.win>
      * @copyright       David Lienhard
-     * @return          mixed[]
+     * @return          (int|float|string|bool|null)[]
      * @throws          \DavidLienhard\Database\Exception if any mysqli function failed
      */
     public function fetch_row_assoc() : array
@@ -83,7 +83,7 @@ class MysqliResult implements ResultInterface
      * @author          David Lienhard <github@lienhard.win>
      * @copyright       David Lienhard
      * @param           int                 $resulttype     the type of the result
-     * @return          mixed[]|null
+     * @return          (int|float|string|bool|null)[]|null
      * @throws          \DavidLienhard\Database\Exception if any mysqli function failed
      */
     public function fetch_array(int $resulttype = MYSQLI_BOTH) : array|null
@@ -115,7 +115,7 @@ class MysqliResult implements ResultInterface
      *
      * @author          David Lienhard <github@lienhard.win>
      * @copyright       David Lienhard
-     * @return          mixed[]|null
+     * @return          (int|float|string|bool|null)[]|null
      * @throws          \DavidLienhard\Database\Exception if any mysqli function failed
      */
     public function fetch_row() : array|null
@@ -137,7 +137,7 @@ class MysqliResult implements ResultInterface
      * @author          David Lienhard <github@lienhard.win>
      * @copyright       David Lienhard
      * @param           int                 $resulttype     type of array to return
-     * @return          mixed[]
+     * @return          array<int, (int|float|string|bool|null)[]>
      * @throws          \DavidLienhard\Database\Exception if any mysqli function failed
      */
     public function fetch_all(int $resulttype = MYSQLI_NUM) : array
